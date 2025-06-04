5a7b8d468d1dd0971949ea269022cbc8cc7edbdf
evtrnist <evagrandet@ro.ru>
2025-06-04 09:29:33 +0400

    Add Swagger integration and enhance AuthDto with API documentation

--- apps/api/src/app/auth/dto/auth.ts
+++ apps/api/src/app/auth/dto/auth.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class AuthDto {
+    @ApiProperty({ example: 'user@example.com' })
     @IsEmail({}, {message: 'Imvalid email address'})
     email: string;
 
+    @ApiProperty({ example: '12345678' })
     @IsString({message: 'Password must be a string'})
     @MinLength(7, {message: 'Password must be at least 7 characters long'})
     password: string;
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,12 +5,15 @@
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -18,6 +21,21 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('LifeDashboard API')
+    .setDescription(
+      'API для авторизации, работы с дашбордом и данными пользователей',
+    )
+    .setVersion('1.0')
+    .addBearerAuth() // <--- для JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
