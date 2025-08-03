e1d65c33dfdd21ef6916b2dd5f84edb81182d50e
evtrnist <evagrandet@ro.ru>
2025-08-03 12:43:45 +0400

    refactor(widget-layout-settings): format code for consistency and readability

--- apps/front/src/app/settings/widget-layout-settings/widget-layout-settings.service.ts
+++ apps/front/src/app/settings/widget-layout-settings/widget-layout-settings.service.ts
@@ -2,25 +2,22 @@ import { inject, Injectable } from '@angular/core';
 import { Layout } from '@lifestyle-dashboard/config';
 import { LifestyleWidgetConfigService } from '@lifestyle-dashboard/lifestyle-widget-config-service';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class WidgetLayoutSettingsService {
   private readonly lifestyleWidgetConfigService = inject(
     LifestyleWidgetConfigService,
   );
 
-
   public readonly $config = this.lifestyleWidgetConfigService.$config;
 
-  public saveWidgetLayoutSettings(
-    layout: Layout,
-  ): void {
+  public saveWidgetLayoutSettings(layout: Layout): void {
     const config = this.$config();
 
     if (!config) {
-      this.lifestyleWidgetConfigService.createConfig({layout});
+      this.lifestyleWidgetConfigService.createConfig({ layout });
       return;
     }
 
-    this.lifestyleWidgetConfigService.updateConfig({layout})
+    this.lifestyleWidgetConfigService.updateConfig({ layout });
   }
 }
