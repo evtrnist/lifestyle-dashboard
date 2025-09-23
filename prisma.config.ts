4be75ce1cb0a013b7d68d653f680a1f1206e4d3a
evtrnist <evagrandet@ro.ru>
2025-09-23 19:49:49 +0400

    refactor(prisma): remove unused Prisma configuration file

--- prisma.config.ts
+++
@@ -1,6 +0,0 @@
-import { definePrismaConfig } from '@prisma/internals';
-import path from 'node:path';
-
-export default definePrismaConfig({
-  schema: path.join(__dirname, 'apps/api/prisma/schema.prisma'),
-});
