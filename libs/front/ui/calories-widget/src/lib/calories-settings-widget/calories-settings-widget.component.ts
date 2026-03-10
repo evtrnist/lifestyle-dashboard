95cee3af26e8738b3d876e63e08518d252dad3d9
evtrnist <evagrandet@ro.ru>
2026-03-10 17:08:21 +0200

    feat(calories-widget): enhance calories settings widget with data injection and initialization

--- libs/front/ui/calories-widget/src/lib/calories-settings-widget/calories-settings-widget.component.ts
+++ libs/front/ui/calories-widget/src/lib/calories-settings-widget/calories-settings-widget.component.ts
@@ -1,8 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  OnInit,
+  Signal,
+} from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TuiLabel, TuiTextfield } from '@taiga-ui/core';
 import { TuiInputNumber } from '@taiga-ui/kit';
 import { WidgetSettingsComponent } from '@lifestyle-dashboard/widget-contracts';
+import { CaloriesWidgetInput } from '../calories-widget-input';
+import { CALORIES_WIDGET_TOKEN } from '../calories-widget.token';
 
 type CaloriesField = 'eaten' | 'burned';
 
@@ -15,9 +24,31 @@ type CaloriesSettingsForm = Record<CaloriesField, FormControl<number | null>>;
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [ReactiveFormsModule, TuiInputNumber, TuiTextfield, TuiLabel],
 })
-export class CaloriesSettingsWidgetComponent implements WidgetSettingsComponent {
+export class CaloriesSettingsWidgetComponent implements WidgetSettingsComponent, OnInit {
   public readonly form = new FormGroup<CaloriesSettingsForm>({
     eaten: new FormControl(0),
     burned: new FormControl(0),
   });
+
+  public readonly widgetData = inject<Signal<CaloriesWidgetInput>>(CALORIES_WIDGET_TOKEN);
+
+  public readonly $size = computed(() => this.widgetData()?.size);
+
+  public readonly $caloriesData = computed(() => this.widgetData()?.data);
+
+  public ngOnInit(): void {
+    this.setWidgetData();
+  }
+
+  private setWidgetData(): void {
+    const caloriesData = this.$caloriesData();
+
+    if (!caloriesData) {
+      return;
+    }
+    this.form.setValue({
+      eaten: caloriesData.eaten,
+      burned: caloriesData.burned,
+    });
+  }
 }
